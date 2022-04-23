43b7ac67d78aebd2655a3bd7b577847b6b96445d
ta121443 <ryo_uchi4_20@outlook.jp>
2022-04-23 23:54:22 +0900

    change file name

--- test.cpp
+++ test.cpp
@@ -3,16 +3,5 @@ using namespace std;
 #define rep(i, n) for(int i=0; i < (int)(n); i++)
 
 int main(){
-    int N, K;
-    cin >> N >> K;
-
-    vector<string> S(N);
-    rep(i, N){
-        cin >> S.at(i);
-    }
-
-    vector<int> same_num(26, 0);
     
-
-
 }
